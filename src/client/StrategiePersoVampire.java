4e06d0b1914f32c03c7fc05b349f3bfa7bef1307
Guillaume <Guillaume@Guihome>
2016-01-05 19:13:08 +0100

    stratVamp

---
+++ src/client/StrategiePersoVampire.java
@@ -0,0 +1,79 @@
+package client;
+
+
+import java.awt.Point;
+import java.rmi.RemoteException;
+import java.util.HashMap;
+
+import logger.LoggerProjet;
+import serveur.IArene;
+import serveur.element.Element;
+import serveur.element.Personnage;
+import serveur.element.Potion;
+import utilitaires.Calculs;
+import utilitaires.Constantes;
+
+/**
+ * Strategie d'un personnage. 
+ */
+public class StrategiePersoVampire extends StrategiePersonnage{
+
+	public StrategiePersoVampire(String ipArene, int port, String ipConsole, Personnage perso, int nbTours,
+			Point position, LoggerProjet logger) {
+		super(ipArene, port, ipConsole, perso, nbTours, position, logger);
+	}
+	
+	/** 
+	 * Decrit la strategie.
+	 * Les methodes pour evoluer dans le jeu doivent etre les methodes RMI
+	 * de Arene et de ConsolePersonnage. 
+	 * @param voisins element voisins de cet element (elements qu'il voit)
+	 * @throws RemoteException
+	 */
+	public void executeStrategie(HashMap<Integer, Point> voisins) throws RemoteException {
+		// arene
+		IArene arene = console.getArene();
+		
+		// reference RMI de l'element courant
+		int refRMI = 0;
+		
+		// position de l'element courant
+		Point position = null;
+		
+		try {
+			refRMI = console.getRefRMI();
+			position = arene.getPosition(refRMI);
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		if (voisins.isEmpty()) { // je n'ai pas de voisins, j'erre
+			console.setPhrase("J'erre...");
+			arene.deplace(refRMI, 0); 
+			
+		} else {
+			int refCible = Calculs.chercheElementProche(position, voisins);
+			int distPlusProche = Calculs.distanceChebyshev(position, arene.getPosition(refCible));
+
+			Element elemPlusProche = arene.elementFromRef(refCible);
+			if(distPlusProche <= Constantes.DISTANCE_MIN_INTERACTION) { // si suffisamment proches
+				// j'interagis directement
+				if(elemPlusProche instanceof Potion) { // potion
+					// ramassage
+					console.setPhrase("Je ramasse une potion");
+					arene.ramassePotion(refRMI, refCible);
+
+				} else { // personnage
+					// duel
+					console.setPhrase("Je suce le sang de " + elemPlusProche.getNom());
+					arene.lanceAttaque(refRMI, refCible);
+				}
+				
+			} else { // si voisins, mais plus eloignes
+				// je vais vers le plus proche
+				console.setPhrase("Je vais vers mon voisin " + elemPlusProche.getNom());
+				arene.deplace(refRMI, refCible);
+			}
+		}
+	}
+}
