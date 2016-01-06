980990f81a0f300726639e2eb3bff1e373426b9e
Guillaume <Guillaume@Guihome>
2016-01-06 10:20:34 +0100

    CommitTest

--- src/serveur/element/Vampire.java
+++ src/serveur/element/Vampire.java
@@ -5,7 +5,13 @@ import java.util.HashMap;
 public class Vampire extends Personnage{
 	
 	private static final long serialVersionUID = 1L;
-
+	
+	/**
+	 * 
+	 * @param nom
+	 * @param groupe
+	 * @param caracts
+	 */
 	public Vampire(String nom, String groupe, HashMap<Caracteristique, Integer> caracts) {
 		super(nom, groupe, caracts);
 	}
